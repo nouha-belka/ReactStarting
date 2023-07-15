56a31c21206196206b8084f66c0ccac53ad3e614
nouha belkacemi <84313345+nouha-belka@users.noreply.github.com>
2023-07-15 00:58:16 -0700

    starting new idea

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import ProfileCard from "./ProfileCard";
-import AlexaImage from "./images/alexa.png";
-import CortanaImage from "./images/cortana.png";
-import SiriImage from "./images/siri.png";
 
-function App(){
-    return (
-        <div>
-            {/* <img src={AlexaImage} alt="" /> */}
-            <div>Personal Digital Assistants</div>
-            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}/>
-            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage}/>
-            <ProfileCard title="Siri" handle="@siri01" image={SiriImage}/>
-        </div>
-    );
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    const handelClick = () =>{
+        setCount(count + 1);
+    };
+
+  return (
+    <div>
+        <button onClick={handelClick}>Add Animal</button>
+        <div>Number of animals: {count}</div>
+    </div>
+  );
 }
 export default App;
---
+++ src/AppExample.js
@@ -0,0 +1,37 @@
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+function App() {
+  return (
+    <div>
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <p class="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+export default App;
